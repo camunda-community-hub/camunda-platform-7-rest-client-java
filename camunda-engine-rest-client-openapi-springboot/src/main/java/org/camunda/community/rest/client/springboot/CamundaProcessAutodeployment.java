f6d9889acdd42152861f96f03ef3bcb0724c7d06
Bernd Ruecker <bernd.ruecker@camunda.com>
2022-07-11 16:15:52 +0200

    Switched to more manual resource loading because of problems with default value

--- camunda-engine-rest-client-openapi-springboot/src/main/java/org/camunda/community/rest/client/springboot/CamundaProcessAutodeployment.java
+++ camunda-engine-rest-client-openapi-springboot/src/main/java/org/camunda/community/rest/client/springboot/CamundaProcessAutodeployment.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Component;
+import org.springframework.core.io.support.ResourcePatternResolver;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
@@ -23,25 +23,38 @@ public class CamundaProcessAutodeployment {
     @Autowired
     private DeploymentApi deploymentApi;
 
+    @Autowired
+    private ResourcePatternResolver patternResolver;
+
     // TODO Possible extension: Provide a @Deployment annotation like Spring Zeebe
-    @Value("${camunda.autoDeploy.bpmnResources:'classpath*:**/*.bpmn'")
-    private Resource[] bpmnResources;
+    @Value("${camunda.autoDeploy.bpmnResources:}")
+    private String bpmnResourcesPattern;
 
-    @Value("${camunda.autoDeploy.dmnResources:'classpath*:**/*.dmn'")
-    private Resource[] dmnResources;
+    @Value("${camunda.autoDeploy.dmnResources:}")
+    private String dmnResourcesPattern;
 
-    @Value("${camunda.autoDeploy.formResources:'classpath*:**/*.form'")
-    private Resource[] formResources;
+    @Value("${camunda.autoDeploy.formResources:")
+    private String formResourcesPattern;
 
     @Value("${spring.application.name:spring-app}")
     private String applicationName;
 
     @PostConstruct
     public void deployCamundaResources() throws IOException, ApiException {
+        if (bpmnResourcesPattern==null || bpmnResourcesPattern.length()==0) {
+            bpmnResourcesPattern = "classpath*:**/*.bpmn"; // Not sure why the default mechanism in @Value makes problems - but this works!
+        }
+        if (dmnResourcesPattern==null || dmnResourcesPattern.length()==0) {
+            dmnResourcesPattern = "classpath*:**/*.dmn";
+        }
+        if (formResourcesPattern==null || formResourcesPattern.length()==0) {
+            formResourcesPattern = "classpath*:**/*.form";
+        }
+
         List<Resource> resourcesToDeploy = new ArrayList<>();
-        resourcesToDeploy.addAll(Arrays.asList( bpmnResources ));
-        resourcesToDeploy.addAll(Arrays.asList( dmnResources ));
-        resourcesToDeploy.addAll(Arrays.asList( formResources ));
+        resourcesToDeploy.addAll(Arrays.asList( patternResolver.getResources(bpmnResourcesPattern) ));
+        resourcesToDeploy.addAll(Arrays.asList( patternResolver.getResources(dmnResourcesPattern) ));
+        resourcesToDeploy.addAll(Arrays.asList( patternResolver.getResources(formResourcesPattern) ));
 
         for (Resource camundaResource: resourcesToDeploy) {
             deploymentApi.createDeployment(
