a416b45511e06b59b6aae7da78b8bbbf09bf0ac5
Jonathan Lukas <jonathan.lukas@camunda.com>
2024-06-20 06:17:58 +0200

    added password protection properties

--- camunda-engine-rest-client-openapi-springboot/src/main/java/org/camunda/community/rest/client/springboot/CamundaOpenApiStarter.java
+++ camunda-engine-rest-client-openapi-springboot/src/main/java/org/camunda/community/rest/client/springboot/CamundaOpenApiStarter.java
@@ -9,13 +9,28 @@ import org.springframework.context.annotation.Bean;
 public class CamundaOpenApiStarter {
 
   private final String basePath;
+  private final String username;
+  private final String password;
 
-  public CamundaOpenApiStarter(@Value("${camunda.bpm.client.base-url:#{null}}") String basePath) {
+  public CamundaOpenApiStarter(
+      @Value("${camunda.bpm.client.base-url:#{null}}") String basePath,
+      @Value("${camunda.bpm.client.basic-auth.username:#{null}}") String username,
+      @Value("${camunda.bpm.client.basic-auth.password:#{null}}") String password) {
     this.basePath = basePath;
+    this.username = username;
+    this.password = password;
   }
 
   public String getBasePath() {
     return basePath;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getPassword() {
+    return password;
   }
 
   @Bean
@@ -23,6 +38,12 @@ public class CamundaOpenApiStarter {
     ApiClient client = new ApiClient();
     if (basePath != null) {
       client.setBasePath(basePath);
+    }
+    if (username != null) {
+      client.setUsername(username);
+    }
+    if (password != null) {
+      client.setPassword(password);
     }
     return client;
   }
